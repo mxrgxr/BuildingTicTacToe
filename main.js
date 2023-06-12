c2fa1b01cbf88a46081412278e8830caa7b3cc43
mxrgxr <mgxrcias@gmail.com>
2023-06-12 00:41:17 -0700

    store winning combos in array

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ const colors = {
     '-1': 'green'
 }
 
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [0, 4, 8]
+]
+
 let board;
 let turn;
 let winner;
